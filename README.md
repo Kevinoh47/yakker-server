dc30f2354f924efbe08f4b0a5f9bb342e8792198
Kevinoh47 <kohok47@gmail.com>
2019-01-20 14:14:30 -0800

    working server and client. Link to client sandbox in README

---
+++ README.md
@@ -0,0 +1,13 @@
+![CF](http://i.imgur.com/7v5ASc8.png) LAB
+=================================================
+
+## Lab 38 Socket.IO yakker server and clients
+
+### Author: Kevin O'Halloran
+
+### Links and Resources
+* [repo](https://github.com/Kevinoh47/yakker-server)
+* [client codesandbox](https://codesandbox.io/s/oxmly4r0xq)
+
+### Modules
+#### `auth.js`
--- server/server.js
+++ server/server.js
@@ -4,17 +4,31 @@ const fs = require('fs');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const io = require('socket.io')(PORT);
+
+let history = [];
 
 io.on('connection', socket => {
 
   console.log('Connected', socket.id);
 
-  socket.on('troll', (payload) => {
+  socket.on('chatter', (payload) => {
     console.log('broadcast', payload);
-    socket.broadcast.emit('incoming', payload);
+    history.push(payload);
+    console.log('history: ', history);
+    let newPayload = {payload: payload, history: [...history]}
+    socket.broadcast.emit('incoming', newPayload);
   });
+
+  // do i need to make payload an object with .payload and .history?
+  // Or can i handle that in the client state?
+  // socket.on('chatter', (payload) => {
+  //   console.log('broadcast', payload);
+  //   history.push(payload);
+  //   console.log('history: ', history);
+  //   socket.broadcast.emit('incoming', payload);
+  // });
 
 });
